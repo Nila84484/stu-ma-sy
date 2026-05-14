f70f51b7b690276692ea7a2b2e8c9d34f10a7a2e
Nila84484 <jeyakiruba1978@gmail.com>
2026-05-14 14:57:17 +0300

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -17,23 +17,4 @@ export default function App() {
       </Routes>
     </BrowserRouter>
   );
-}import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-function Login() {
-  return <h1>Login Page</h1>;
-}
-
-function Dashboard() {
-  return <h1>Dashboard</h1>;
-}
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
 }
